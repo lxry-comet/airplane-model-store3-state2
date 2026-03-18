b0e79aa6cf64521c2a714f879d7eaf1875bbe579
lxry-comet <durdentagirowich@gmail.com>
2026-03-18 13:44:38 +0100

    state-filtration

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,7 +3,7 @@ import '@/App.css'
 // import planes from '../../json/planes.json'
 import planes from '@/json/planes.json'
 
-import React, { Component } from "react";
+import React, { Component } from 'react'
 
 import helicopters from '@/json/helicopters.json'
 // import PlanesList from '../PlanesList/PlanesList.jsx'
@@ -14,53 +14,72 @@ import Section from '@/components/Section/Section.jsx'
 import css from './App.module.css'
 
 export class App extends Component {
+	state = {
+		isPlain: false,
+		isHelicopter: false
+	}
 
 	allFiltration = () => {
 		console.log('all')
+		this.setState({
+			isPlain: true,
+			isHelicopter: true
+		})
 	}
 	planeFiltration = () => {
 		console.log('planeFiltration')
+		this.setState({
+			isPlain: true,
+			isHelicopter: false
+		})
 	}
 	helicopterFiltration = () => {
 		console.log('helicopterFiltration')
+		this.setState({
+			isPlain: false,
+			isHelicopter: true
+		})
 	}
-render() {
-	return (
-		<>
-			{/*//!  Filter */}
-			<div className={css.filterBox}>
-				<button
-					className={css.buttonAllFiltration}
-					type='button'
-					// onClick={this.allFiltration}
-					onClick={() => { console.log("all") }}
-				>
-					ВСІ
-				</button>
+	render() {
+		return (
+			<>
+				{/*//!  Filter */}
+				<div className={css.filterBox}>
+					<button
+						className={css.buttonAllFiltration}
+						type='button'
+						onClick={this.allFiltration}
+					>
+						ВСІ
+					</button>
 
-				<button
-					className={css.buttonPlaneFiltration}
-					type='button'
-					// onClick={this.planeFiltration}
-				>
-					Літаки
-				</button>
+					<button
+						className={css.buttonPlaneFiltration}
+						type='button'
+						onClick={this.planeFiltration}
+					>
+						Літаки
+					</button>
 
-				<button
-					className={css.buttonHelicopterFiltration}
-					type='button'
-					// onClick={this.helicopterFiltration}
-				>
-					Вертольоти
-				</button>
-			</div>
-			<Section isOn={true} title='Магазин моделей літаків'>
-				<PlanesList items={planes} />
-			</Section>
+					<button
+						className={css.buttonHelicopterFiltration}
+						type='button'
+						onClick={this.helicopterFiltration}
+					>
+						Вертольоти
+					</button>
+				</div>
+				<Section isOn={this.state.isPlain} title='Магазин моделей літаків'>
+					<PlanesList items={planes} />
+				</Section>
 
-			<Section isOn={true} title='Магазин моделей вертольотів'>
-				<PlanesList items={helicopters} />
-			</Section>
-		</>
-	)}
+				<Section
+					isOn={this.state.isHelicopter}
+					title='Магазин моделей вертольотів'
+				>
+					<PlanesList items={helicopters} />
+				</Section>
+			</>
+		)
+	}
 }
