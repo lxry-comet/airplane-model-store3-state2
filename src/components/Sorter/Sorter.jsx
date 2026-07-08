cfab937c99149093e739c62edd9d39e3a2a5cfaf
lxry-comet <durdentagirowich@gmail.com>
2026-07-08 23:23:48 +0200

    update

--- src/components/Sorter/Sorter.jsx
+++ src/components/Sorter/Sorter.jsx
@@ -1,60 +1,73 @@
-// import React, { Component } from "react";
+import React, { Component } from "react";
 import css from './Sorter.module.css'
 
-export function Sorter({ inputSearch, onHandleChangeInputSearchValue }) {
-	return (
-		<div className={css.sorterBox}>
-			<h3 className={css.formTitle}>Вибір параметра для пошуку/фільтрації:</h3>
-			<form>
-				<label>
-					<input
-						type='radio'
-						name='sort'
-						value='brief'
-						// checked={sort === 'brief'}
-						// onChange={this.handleChange}
-					/>
-					Назва
-				</label>
-				<label>
-					<input
-						type='radio'
-						name='sort'
-						value='nickname'
-						// checked={experience === 'nickname'}
-						// onChange={this.handleChange}
-					/>
-					Прізвисько
-				</label>
-				<label>
-					<input
-						type='radio'
-						name='sort'
-						value='country'
-						// checked={experience === 'country'}
-						// onChange={this.handleChange}
-					/>
-					Країна виробник
-				</label>
-
-				<label>
-					<input
-						type='radio'
-						name='sort'
-						value='year'
-						// checked={experience === 'year'}
-						// onChange={this.handleChange}
-					/>
-					Рік випуску
-				</label>
-			</form>
-			<input
-				className={css.inputSearch}
-				type='text'
-				placeholder='Введіть назву ЛА'
-				value={inputSearch}
-				onChange={onHandleChangeInputSearchValue}
-			/>
-		</div>
-	)
+export class Sorter extends Component{
+		state = {
+		sort: 'brief'
+	}
+	handleChange = (e) => {
+		this.setState({
+			sort: e.target.value
+		})
+	}
+	render() {
+		const {inputSearch, onHandleChangeInputSearchValue} = this.props;
+		const { sort } = this.state;
+		return (
+			<div className={css.sorterBox}>
+				<h3 className={css.formTitle}>
+					Вибір параметра для пошуку/фільтрації:
+				</h3>
+				<form>
+					<label>
+						<input
+							type='radio'
+							name='sort'
+							value='brief'
+							checked={sort === 'brief'}
+							onChange={this.handleChange}
+						/>
+						Назва
+					</label>
+					<label>
+						<input
+							type='radio'
+							name='sort'
+							value='nickname'
+							checked={sort === 'nickname'}
+							onChange={this.handleChange}
+						/>
+						Прізвисько
+					</label>
+					<label>
+						<input
+							type='radio'
+							name='sort'
+							value='country'
+							checked={sort === 'country'}
+							onChange={this.handleChange}
+						/>
+						Країна виробник
+					</label>
+					<label>
+						<input
+							type='radio'
+							name='sort'
+							value='year'
+							checked={sort === 'year'}
+							onChange={this.handleChange}
+						/>
+						Рік випуску
+					</label>
+				</form>
+				<input
+					className={css.inputSearch}
+					type='text'
+					placeholder='Введіть назву ЛА'
+					value={inputSearch}
+					onChange={onHandleChangeInputSearchValue}
+				/>
+			</div>
+		)
+	}
 }
