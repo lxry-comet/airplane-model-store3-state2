0a5d6c8f69c739c9806449a40ee45caf643a331d
lxry-comet <durdentagirowich@gmail.com>
2026-07-28 17:32:27 +0200

    debaunce

--- src/components/App/AppSearchDebounce.jsx
+++ src/components/App/AppSearchDebounce.jsx
@@ -2,11 +2,34 @@ import React, { Component } from 'react'
 import css from './AppSearchDebounce.module.css'
 import data from '@/json/cards_10-10.json'
 
+
+import debounce from "lodash.debounce";
+
+
 export class AppSearchDebounce extends Component {
 	state = {
 		inputValue: '',
 		filteredArray: data
 	}
+	// handleChange = debounce((event) => { }, 500);//! так не працює
+
+	//! 3.Винесимо всю логіку фільтрації в окремий метод:
+  performSearch = textInput => {		
+
+		const filteredArray = data.filter(item => item.title.toLowerCase().includes(textInput.toLowerCase().trim()))
+		
+		console.log('Відфільтрований масив: ', filteredArray)
+		
+		this.setState({
+			filteredArray
+		})
+	}
+	//! 4.Створюємо debounce як class property:
+	debouncedSearch = debounce((text) => {
+    console.log("⏰debounce_text", text);
+    this.performSearch(text);
+  }, 500);
+
 	handleChange = event => {
 		//! Деструктуризуємо:
 		const { name, value } = event.currentTarget
@@ -19,13 +42,24 @@ export class AppSearchDebounce extends Component {
 		this.setState({
 			[name]: value
 		})
-		//! Логіка фільтрації
-		const filteredArray = data.filter(item => item.title.toLowerCase().includes(value.toLowerCase().trim()))
-		console.log('Відфільтрований масив: ', filteredArray)
-		this.setState({
-			filteredArray
-		})
+		//! _____________Логіка фільтрації___________
+		// const filteredArray = data.filter(item => item.title.toLowerCase().includes(value.toLowerCase().trim()))
+		// console.log('Відфільтрований масив: ', filteredArray)
+		// this.setState({
+		// 	filteredArray
+		// })
+		//! _________________________________________
+
+		//! 6.2 Запуск debounce з логікою фільтрації:
+    this.debouncedSearch(value);
+
 	}
+	//! 5.Припинення debounce:
+ 	componentWillUnmount() {
+    this.debouncedSearch.cancel();
+  };
+
+
 	render() {
 		const { inputValue, filteredArray } = this.state
 
@@ -46,6 +80,7 @@ export class AppSearchDebounce extends Component {
 					name='inputValue'
 					value={inputValue}
 					onChange={this.handleChange}
+					// onChange={debounce(this.handleChange, 500)} //! так не працює
 				/>
 				<ul className={css.cards}>
 					{filteredArray.map(item => (
